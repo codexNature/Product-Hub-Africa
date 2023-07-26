3b8865391810fa836a558f185d20ff6c451477b2
naturesola <100572919+naturesola@users.noreply.github.com>
2023-07-26 15:45:13 +0100

    resynced

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 $("#footer").load("footer.html"); 
+
+// <!-- <li><a href="C:\Users\USER\Desktop\ProBono Projects\Product Hub Africa\index.html" class="nav-link px-5 text-white">Homeses</a></li>
+// <li><a href="C:\Users\USER\Desktop\ProBono Projects\Product Hub Africa\about us.html" class="nav-link px-5 text-white">About Us</a></li>
+// <li><a href="C:\Users\USER\Desktop\ProBono Projects\Product Hub Africa\blog page.html" class="nav-link px-5 text-white">Blog</a></li>
+// <li><a href="C:\Users\USER\Desktop\ProBono Projects\Product Hub Africa\contact us.html" class="nav-link px-5 text-white">Contact Us</a></li> -->
